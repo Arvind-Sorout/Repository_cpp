4b0f7d146eeee61537491114ea97a6c52d65f0bf
Arvind-Sorout <arvindsorout66@gmail.com>
2023-01-27 13:44:55 +0530

    Add a basic folder on github

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "" << endl;
+    cout << "Help for setting Visual Studio code for first time:" << endl;
+    cout << "   1. Go to Extension and download ## C/C++ IntelliSense, debugging, and code browsing. ## " << endl;
+    cout << "   2. Go to Extension and Download ## Runner ## " << endl;
+    cout << "   3. Go to Extension and ## C/C++ Extension Pack ## " << endl;
+    cout << "   4. Go to Extension and ## CMake and CMake Tools ## " << endl;
+    cout << "" << endl;
+}
